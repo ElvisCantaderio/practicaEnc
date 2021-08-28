a8e548ac08a60177ff6aabaa789846d3b3f13ccf
Cantaderio <VC20009@ues.edu.sv>
2021-08-28 16:44:55 -0600

    este es un comentario

---
+++ hola/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define PI 3.1416
+/*
+ int main(){
+    int nacimiento, actual, edad;
+ 
+    printf("Introduce tu ano de nacimiento: ");
+    scanf("%i",&nacimiento);
+ 
+    getchar();
+ 
+    printf("Introduce el ano actual: ");
+    scanf("%i",&actual);
+ 
+    getchar();
+ 
+    edad = actual - nacimiento;
+ 
+    printf("Tienes aproximadamente %i anos\n\nPresiona Enter para Salir", edad);
+ 
+    getchar();
+    return 0;
+}   
+ */
+/*
+ int main(){
+    char nombre[25],letra='d';
+    printf("Ingrese su nombre completo!!");
+    //scanf("%s",&nombre);
+    gets(nombre);
+    printf("su nombre es: %s",nombre);
+    printf("Ingrese una letra!!");
+    scanf("%c",&nombre);
+    printf("su letra es: %c",letra);
+    return 0;
+}
+ int main (){
+     const double pi=13;
+    float producto=0;
+    int radio=0;
+   printf("cual es el radio del circulo??");
+   scanf("%f",&radio);
+   producto=pi*radio;
+   printf("su resultado es: %f",producto);
+}
+ */
+
+
+
+ 
